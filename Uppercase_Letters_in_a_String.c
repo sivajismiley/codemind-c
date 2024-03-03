069974d8b968604f8d74ba8feb70ac4a9b21e8e2
sivajismiley <147472854+sivajismiley@users.noreply.github.com>
2024-03-03 12:03:50 +0530

    #include<stdio.h> int main() {     char s[1000];     int c=0,i;     scanf("%[^ ]",s);     for(i=0;s[i]!=NULL;i++)     {         if(s[i]>=65&&s[i]<=90){         c++;    }    }     printf("%d",c); }

---
+++ Uppercase_Letters_in_a_String.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char s[1000];
+    int c=0,i;
+    scanf("%[^
+]",s);
+    for(i=0;s[i]!=NULL;i++)
+    {
+        if(s[i]>=65&&s[i]<=90){
+        c++;
+   }
+   }
+    printf("%d",c);
+}
